b4a47ffc9d97f99bfc03b595c530d75e8060424d
Fred1340 <hphch2@hanmail.net>
2021-12-30 09:44:52 +0900

    chapter5 finished

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,21 @@
-import Counter from "./Counter"
-import EventPractice from "./EventPractice";
-import Say from "./Say";
+import { Component } from "react";
+// import Counter from "./Counter"
+// import EventPractice from "./EventPractice";
+// import Say from "./Say";
+import ScrollBox from "./ScrollBox";
+// import VaildationSample from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice/>
-};
+class App extends Component {
+  render() {                                                                                                                                                                                                                                                                                                                                                                                              
+    return( 
+    <div>
+      <ScrollBox ref={(ref)=>this.scrollBox=ref}/>
+      <button onClick={()=>this.scrollBox.scrollToBottom()}>
+        맨 밑으로
+      </button>
+    </div>
+    );
+  };
+}  
 
-export default App;
+export default App;                                   
